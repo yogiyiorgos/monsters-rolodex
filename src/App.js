79b24f94b2fac94362ed426d6f530965ba8da47c
yogiyiorgos <georgefil@outlook.com>
2022-06-10 13:57:56 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Yiorgos', lastName: 'Yogi' },
+      company: 'Geolita',
+    };
+  }
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}! I work at{' '}
+            {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState( () => {
+                return {
+                  name: { firstName: 'Sarah', lastName: 'Zarabina' }
+                }
+              },
+              () => {}{
+                ,
+              });
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

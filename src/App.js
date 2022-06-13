9445c8fd8314f7265a102d5a3dd50c20f9a73c52
yogiyiorgos <georgefil@outlook.com>
2022-06-13 15:24:54 +0200

    Only with class components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,74 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+// import { useState } from 'react';
+
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+// const App = () => {
+//   const [searchField, setSearchField] = useState('');  // [value, setValue]
+
+//   const onSearchChange = (event) => {
+//     const searchFieldString = event.target.value.toLocaleLowerCase();
+//     setSearchField(searchFieldString);
+//     };
+//   };
+//   return (
+//     <div className='App'>
+//       <h1 className='app-title'>Monsters Rolodex</h1>
+//       <SearchBox
+//         className='monsters-search-box'
+//         onChangeHandler={onSearchChange}
+//         placeholder='Search Monsters'
+//       />
+//       <CardList monsters={filteredMonsters} />
+//     </div>
+//   );
+// }
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: { firstName: 'Yiorgos', lastName: 'Yogi' },
-      company: 'Geolita',
+      monsters: [],
+      searchField: '',
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}! I work at{' '}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState( () => {
-                return {
-                  name: { firstName: 'Sarah', lastName: 'Zarabina' }
-                }
-              },
-              () => {}{
-                ,
-              });
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='Search Monsters'
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
